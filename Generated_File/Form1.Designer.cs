cf1b08da04d8c792169f2d280f8981b6fd0ef189
Abdel-Rahman Ali <AAli@domain.com>
2020-06-03 10:17:54 +0200

    Add project files.

---
+++ Generated_File/Form1.Designer.cs
@@ -0,0 +1,442 @@
+﻿namespace Generated_File
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtsrPsw = new System.Windows.Forms.TextBox();
+            this.txtsrUser = new System.Windows.Forms.TextBox();
+            this.txtsrPor = new System.Windows.Forms.TextBox();
+            this.txtsrDb = new System.Windows.Forms.TextBox();
+            this.SrCmb = new System.Windows.Forms.ComboBox();
+            this.txtsrServer = new System.Windows.Forms.TextBox();
+            this.txtsrname = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.TrgCmb = new System.Windows.Forms.ComboBox();
+            this.txtTrgPs = new System.Windows.Forms.TextBox();
+            this.txtTrgUs = new System.Windows.Forms.TextBox();
+            this.txtTRPo = new System.Windows.Forms.TextBox();
+            this.txtTrDB = new System.Windows.Forms.TextBox();
+            this.txtTrSrV = new System.Windows.Forms.TextBox();
+            this.txtTrgNam = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.txtsrPsw);
+            this.panel1.Controls.Add(this.txtsrUser);
+            this.panel1.Controls.Add(this.txtsrPor);
+            this.panel1.Controls.Add(this.txtsrDb);
+            this.panel1.Controls.Add(this.SrCmb);
+            this.panel1.Controls.Add(this.txtsrServer);
+            this.panel1.Controls.Add(this.txtsrname);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(304, 545);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(69, 468);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 45);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Save SRC";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // txtsrPsw
+            // 
+            this.txtsrPsw.Location = new System.Drawing.Point(93, 411);
+            this.txtsrPsw.Name = "txtsrPsw";
+            this.txtsrPsw.Size = new System.Drawing.Size(121, 20);
+            this.txtsrPsw.TabIndex = 14;
+            // 
+            // txtsrUser
+            // 
+            this.txtsrUser.Location = new System.Drawing.Point(93, 358);
+            this.txtsrUser.Name = "txtsrUser";
+            this.txtsrUser.Size = new System.Drawing.Size(121, 20);
+            this.txtsrUser.TabIndex = 13;
+            // 
+            // txtsrPor
+            // 
+            this.txtsrPor.Location = new System.Drawing.Point(93, 294);
+            this.txtsrPor.Name = "txtsrPor";
+            this.txtsrPor.Size = new System.Drawing.Size(121, 20);
+            this.txtsrPor.TabIndex = 12;
+            // 
+            // txtsrDb
+            // 
+            this.txtsrDb.Location = new System.Drawing.Point(93, 246);
+            this.txtsrDb.Name = "txtsrDb";
+            this.txtsrDb.Size = new System.Drawing.Size(121, 20);
+            this.txtsrDb.TabIndex = 11;
+            // 
+            // SrCmb
+            // 
+            this.SrCmb.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.SrCmb.FormattingEnabled = true;
+            this.SrCmb.Items.AddRange(new object[] {
+            "MARIADB",
+            "MSSQLNATIVE"});
+            this.SrCmb.Location = new System.Drawing.Point(93, 195);
+            this.SrCmb.Name = "SrCmb";
+            this.SrCmb.Size = new System.Drawing.Size(154, 21);
+            this.SrCmb.TabIndex = 10;
+            // 
+            // txtsrServer
+            // 
+            this.txtsrServer.Location = new System.Drawing.Point(93, 131);
+            this.txtsrServer.Name = "txtsrServer";
+            this.txtsrServer.Size = new System.Drawing.Size(121, 20);
+            this.txtsrServer.TabIndex = 9;
+            // 
+            // txtsrname
+            // 
+            this.txtsrname.Location = new System.Drawing.Point(93, 69);
+            this.txtsrname.Name = "txtsrname";
+            this.txtsrname.Size = new System.Drawing.Size(121, 20);
+            this.txtsrname.TabIndex = 8;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(21, 411);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Password";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(21, 358);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(59, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "User Name";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(21, 249);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Database";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(21, 302);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Port";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 198);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Type";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Server";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(90, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Source Data";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.TrgCmb);
+            this.panel2.Controls.Add(this.txtTrgPs);
+            this.panel2.Controls.Add(this.txtTrgUs);
+            this.panel2.Controls.Add(this.txtTRPo);
+            this.panel2.Controls.Add(this.txtTrDB);
+            this.panel2.Controls.Add(this.txtTrSrV);
+            this.panel2.Controls.Add(this.txtTrgNam);
+            this.panel2.Controls.Add(this.label9);
+            this.panel2.Controls.Add(this.label10);
+            this.panel2.Controls.Add(this.label11);
+            this.panel2.Controls.Add(this.label12);
+            this.panel2.Controls.Add(this.label13);
+            this.panel2.Controls.Add(this.label14);
+            this.panel2.Controls.Add(this.label15);
+            this.panel2.Controls.Add(this.label16);
+            this.panel2.Location = new System.Drawing.Point(628, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(307, 545);
+            this.panel2.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(82, 468);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 45);
+            this.button2.TabIndex = 23;
+            this.button2.Text = "Save TRG";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // TrgCmb
+            // 
+            this.TrgCmb.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.TrgCmb.FormattingEnabled = true;
+            this.TrgCmb.Items.AddRange(new object[] {
+            "MARIADB",
+            "MSSQLNATIVE"});
+            this.TrgCmb.Location = new System.Drawing.Point(116, 179);
+            this.TrgCmb.Name = "TrgCmb";
+            this.TrgCmb.Size = new System.Drawing.Size(136, 21);
+            this.TrgCmb.TabIndex = 22;
+            // 
+            // txtTrgPs
+            // 
+            this.txtTrgPs.Location = new System.Drawing.Point(116, 400);
+            this.txtTrgPs.Name = "txtTrgPs";
+            this.txtTrgPs.Size = new System.Drawing.Size(100, 20);
+            this.txtTrgPs.TabIndex = 21;
+            // 
+            // txtTrgUs
+            // 
+            this.txtTrgUs.Location = new System.Drawing.Point(116, 344);
+            this.txtTrgUs.Name = "txtTrgUs";
+            this.txtTrgUs.Size = new System.Drawing.Size(100, 20);
+            this.txtTrgUs.TabIndex = 20;
+            // 
+            // txtTRPo
+            // 
+            this.txtTRPo.Location = new System.Drawing.Point(116, 288);
+            this.txtTRPo.Name = "txtTRPo";
+            this.txtTRPo.Size = new System.Drawing.Size(100, 20);
+            this.txtTRPo.TabIndex = 19;
+            // 
+            // txtTrDB
+            // 
+            this.txtTrDB.Location = new System.Drawing.Point(116, 235);
+            this.txtTrDB.Name = "txtTrDB";
+            this.txtTrDB.Size = new System.Drawing.Size(100, 20);
+            this.txtTrDB.TabIndex = 18;
+            // 
+            // txtTrSrV
+            // 
+            this.txtTrSrV.Location = new System.Drawing.Point(116, 120);
+            this.txtTrSrV.Name = "txtTrSrV";
+            this.txtTrSrV.Size = new System.Drawing.Size(100, 20);
+            this.txtTrSrV.TabIndex = 17;
+            // 
+            // txtTrgNam
+            // 
+            this.txtTrgNam.Location = new System.Drawing.Point(116, 65);
+            this.txtTrgNam.Name = "txtTrgNam";
+            this.txtTrgNam.Size = new System.Drawing.Size(100, 20);
+            this.txtTrgNam.TabIndex = 16;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(33, 400);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(53, 13);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Password";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(33, 347);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(59, 13);
+            this.label10.TabIndex = 14;
+            this.label10.Text = "User Name";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(33, 238);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(53, 13);
+            this.label11.TabIndex = 13;
+            this.label11.Text = "Database";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(33, 291);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(27, 13);
+            this.label12.TabIndex = 12;
+            this.label12.Text = "Port";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(33, 187);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(31, 13);
+            this.label13.TabIndex = 11;
+            this.label13.Text = "Type";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(33, 127);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(39, 13);
+            this.label14.TabIndex = 10;
+            this.label14.Text = "Server";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(33, 65);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(34, 13);
+            this.label15.TabIndex = 9;
+            this.label15.Text = "Name";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(113, 15);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(65, 13);
+            this.label16.TabIndex = 8;
+            this.label16.Text = "Target Data";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(970, 618);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.TextBox txtsrServer;
+        private System.Windows.Forms.TextBox txtsrname;
+        private System.Windows.Forms.ComboBox SrCmb;
+        private System.Windows.Forms.TextBox txtsrDb;
+        private System.Windows.Forms.TextBox txtsrPor;
+        private System.Windows.Forms.TextBox txtsrUser;
+        private System.Windows.Forms.TextBox txtsrPsw;
+        private System.Windows.Forms.TextBox txtTrSrV;
+        private System.Windows.Forms.TextBox txtTrgNam;
+        private System.Windows.Forms.TextBox txtTRPo;
+        private System.Windows.Forms.TextBox txtTrDB;
+        private System.Windows.Forms.TextBox txtTrgUs;
+        private System.Windows.Forms.TextBox txtTrgPs;
+        private System.Windows.Forms.ComboBox TrgCmb;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Generated_File/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Generated_File
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //FileStream inputt = new FileStream(@"E:\Files\trf_supplierdocs_cis_inventory.ktr", FileMode.Open, FileAccess.Read);
+
+            //StreamReader reader = new StreamReader(inputt);
+
+            //XmlDocument xmlDocument = new XmlDocument();
+
+            //xmlDocument.Load(@"E:\Files\trf_supplierdocs_cis_inventory.ktr");
+
+           // Console.WriteLine();
+
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            // get the file 
+            var doc = XDocument.Load(@"E:\Files\test_trans7.ktr");
+
+
+            // to read file property 
+            var elementsToUpdate = doc.Descendants()
+                                      .Where(o => o.Name == "connection" && o.HasElements).FirstOrDefault();
+
+            List<XElement> ilist = new List<XElement>();
+            ilist.Add(elementsToUpdate);
+
+            //update elements value
+
+            foreach (XElement element in ilist)
+            {
+                var name = element.Descendants().Where(z => z.Name == "name").FirstOrDefault();
+                var server = element.Descendants().Where(z => z.Name == "server").FirstOrDefault();
+                var type = element.Descendants().Where(z => z.Name == "type").FirstOrDefault();
+                var database = element.Descendants().Where(z => z.Name == "database").FirstOrDefault();
+                var port = element.Descendants().Where(z => z.Name == "port").FirstOrDefault();
+                var username = element.Descendants().Where(z => z.Name == "username").FirstOrDefault();
+                var password = element.Descendants().Where(z => z.Name == "password").FirstOrDefault();
+
+                name.Value = txtsrname.Text;
+                server.Value = txtsrServer.Text;
+                type.Value = SrCmb.SelectedItem.ToString();
+                database.Value = txtsrDb.Text;
+                port.Value = txtsrPor.Text;
+                username.Value = txtsrUser.Text;
+                password.Value = txtsrPsw.Text;
+
+                // Console.WriteLine(element);
+            }
+
+            // after processing save it in file
+             doc.Save(@"E:\Files\test_trans7.ktr");
+
+            MessageBox.Show("Src Config created ");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // get the file 
+            var doc = XDocument.Load(@"E:\Files\test_trans7.ktr");
+
+
+            // to read file property 
+            var elementsToUpdate = doc.Descendants()
+                                      .Where(o => o.Name == "connection" && o.HasElements).Skip(1).FirstOrDefault();
+
+            List<XElement> ilist = new List<XElement>();
+            ilist.Add(elementsToUpdate);
+
+            //update elements value
+
+            foreach (XElement element in ilist)
+            {
+                var name = element.Descendants().Where(z => z.Name == "name").FirstOrDefault();
+                var server = element.Descendants().Where(z => z.Name == "server").FirstOrDefault();
+                var type = element.Descendants().Where(z => z.Name == "type").FirstOrDefault();
+                var database = element.Descendants().Where(z => z.Name == "database").FirstOrDefault();
+                var port = element.Descendants().Where(z => z.Name == "port").FirstOrDefault();
+                var username = element.Descendants().Where(z => z.Name == "username").FirstOrDefault();
+                var password = element.Descendants().Where(z => z.Name == "password").FirstOrDefault();
+
+                name.Value = txtTrgNam.Text;
+                server.Value = txtTrSrV.Text;
+                type.Value = TrgCmb.SelectedItem.ToString();
+                database.Value = txtTrDB.Text;
+                port.Value = txtTRPo.Text;
+                username.Value = txtTrgUs.Text;
+                password.Value = txtTrgPs.Text;
+
+                // Console.WriteLine(element);
+            }
+
+            // after processing save it in file
+            doc.Save(@"E:\Files\test_trans7.ktr");
+
+            MessageBox.Show("Trg Config created ");
+        }
+    }
+}
